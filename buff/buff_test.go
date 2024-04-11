a0a6f31944b4399c99033d2feabe8a145e25e8c2
mikezzb <mikezhoudev@gmail.com>
2024-04-10 21:08:00 -0400

    feat: add utils

--- buff/buff_test.go
+++ buff/buff_test.go
@@ -3,22 +3,38 @@ package buff
 // test
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/mikezzb/steam-trading-crawler/utils"
 )
 
-func TestBuffCrawler_DoReq(t *testing.T) {
+func InitBuffCrawler(t *testing.T) *BuffCrawler {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		t.Errorf("Error loading .env file: %v", err)
+	}
+
 	c := &BuffCrawler{}
-	c.Init("test")
-	c.DoReq("http://localhost:8080", nil, "GET")
+	c.Init(os.Getenv("BUFF_SECRET"))
+	return c
+}
+
+func TestBuffCrawler_CrawlListings(t *testing.T) {
+	name := "★ M9 Bayonet | Marble Fade (Factory New)"
+	buffCrawler := InitBuffCrawler(t)
+	err := buffCrawler.CrawlItemListings(name, 10, nil)
+	if err != nil {
+		t.Errorf("Failed to crawl item listings: %v", err)
+	}
 }
 
 func TestBuffParser_ParseItemListings(t *testing.T) {
 	name := "★ Karambit | Marble Fade (Factory New)"
-	mockResJsonPath := "mocks/listing_res.json"
+	mockResJsonPath := "mocks/gzip_encode.json"
 	mockRes, err := os.ReadFile(mockResJsonPath)
 	if err != nil {
 		t.Errorf("Failed to read mock response file: %s", mockResJsonPath)
@@ -35,6 +51,15 @@ func TestBuffParser_ParseItemListings(t *testing.T) {
 	if err != nil {
 		t.Errorf("Failed to parse item listings: %v", err)
 	}
-	fmt.Printf("Item: %+v\n", item)
-	fmt.Printf("Listings: %+v\n", listings)
+
+	// save item and listings to JSON files
+	itemJsonPath := "mocks/item.json"
+	if err := utils.WriteJSONToFile(item, itemJsonPath); err != nil {
+		t.Errorf("Failed to write item JSON to file: %v", err)
+	}
+	listingsJsonPath := "mocks/listings.json"
+	if err := utils.WriteJSONToFile(listings, listingsJsonPath); err != nil {
+		t.Errorf("Failed to write listings JSON to file: %v", err)
+	}
+
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,11 @@
 package utils
 
 import (
+	"compress/gzip"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
 	"steam-trading/shared"
 )
 
@@ -11,4 +16,57 @@ func PostFormatListing(name string, listings []shared.Listing) []shared.Listing 
 		listings[i].Rarity = shared.GetListingTier(listings[i])
 	}
 	return listings
+}
+
+// decodes a gzip-encoded reader and returns a decoded reader.
+func DecodeReader(reader io.Reader) (io.ReadCloser, error) {
+	// Attempt to create a gzip reader
+	gzReader, err := gzip.NewReader(reader)
+	if err != nil {
+		// If the input is not gzip-encoded, return the original reader
+		if err == gzip.ErrHeader {
+			return io.NopCloser(reader), nil
+		}
+		return nil, err
+	}
+	return gzReader, nil
+}
+
+func SaveRawResponse(resp *http.Response, path string) error {
+	// Open the file with write access, create if not exists, truncate otherwise
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Decode the response body if gzip-encoded
+	decodedReader, err := DecodeReader(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	defer decodedReader.Close()
+
+	// Copy the decoded response body to the file
+	_, err = io.Copy(file, decodedReader)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// WriteJSONToFile writes data to a JSON file.
+func WriteJSONToFile(data interface{}, filePath string) error {
+	jsonData, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(filePath, jsonData, 0644); err != nil {
+		return err
+	}
+
+	return nil
 }
